2a7bcf7bd20ab7ed712ec29efa5bd12c1e8bb8c5
RenanRSilva <renanrambuls@gmail.com>
2021-11-17 19:54:40 -0300

    cart overlay

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,33 @@
+import classes from "./Modal.module.css";
+import reactDom from "react-dom";
+
+const Backdrop = (props) => {
+  return <div onClick={props.onClose} className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlay");
+
+const Modal = (props) => {
+  return (
+    <>
+      {reactDom.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {reactDom.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
